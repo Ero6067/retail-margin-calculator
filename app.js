cd87b509746596d371f86f7034dbf373a616d827
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-09-23 20:24:14 -0700

    adding calculations are being shown with button click

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 "use strict";
+document
+	.getElementById("calculate-form")
+	.addEventListener("submit", function(e) {
+		console.log("test");
+		retailPrice();
+		e.preventDefault();
+	});
 
-document.getElementById().addEventListener("submit", function(e) {
-	e.preventDefault();
-});
-
-// let calculatePrice = num => {
-let amount = document.getElementById("itemCost");
-const value = parseFloat(amount.value);
+let retailPrice = () => {
+	const buyAmount = document.getElementById("itemCost");
+	const sellAmount = document.getElementById("retailPrice");
+	const value = parseFloat(buyAmount.value);
+	console.log(value);
+	sellAmount.value = calculatePrice(value);
+};
 
 // Calculation and rounding
 let calculatePrice = price => {
 	let num = Math.ceil((price * 2) / 5) * 5;
 	if (num <= 25) {
-		console.log(num - 0.01);
+		return num - 0.01;
 	} else if (num >= 30 && num <= 55) {
-		console.log(num - 5.01);
+		return num - 5.01;
 	} else if (num >= 60 && num <= 100) {
-		console.log(num - 10.01);
+		return num - 10.01;
 	} else if (num >= 105 && num <= 125) {
-		console.log(num - 15.01);
+		return num - 15.01;
 	} else if (num >= 130 && num <= 150) {
-		console.log(num - 20.01);
+		return num - 20.01;
 	} else if (num >= 155 && num <= 175) {
-		console.log(num - 25.01);
+		return num - 25.01;
 	} else if (num >= 180 && num <= 205) {
-		console.log(num - 30.01);
+		return num - 30.01;
 	} else if (num > 205) {
-		console.log("Ask Management");
+		return "Ask Management";
 	}
 };
-
-calculatePrice(37.4);
