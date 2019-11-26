5c4e06e2ec71883cf1aac7fb58dc87084233d8eb
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-11-26 00:07:20 -0800

    fixed footer on bottom using flex

---
+++ mtg.js
@@ -0,0 +1,8 @@
+/**
+ * @name Retail Calculator MTG Controller
+ *
+ * @copyright (C) 2019 Kris Lafrance.
+ * @author Kris Lafrance
+ *
+ */
+"use strict";
---
+++ staff.js
@@ -0,0 +1,8 @@
+/**
+ * @name Retail Calculator Staff Controller
+ *
+ * @copyright (C) 2019 Kris Lafrance.
+ * @author Kris Lafrance
+ *
+ */
+"use strict";
